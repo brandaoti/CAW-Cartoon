b7f9696c5e61dec6d03e3304fc184fcbfea605b3
dannbrandao <dannnbrandao@gmail.com>
2019-04-17 23:53:16 -0300

    Adicionado Script MoveOffSet, usando conjunto ao material, MeshRenderer.

---
+++ Assets/Scripts/Others/MoveOffset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffset : MonoBehaviour {
+
+    private Renderer    mRenderer;
+    private Material    currentMaterial;
+
+    public float        incrementOffset; 
+    public float        speed;
+
+    private float       offSet;
+
+    // Use this for initialization
+    void Start () {
+
+        mRenderer = GetComponent<MeshRenderer>();
+
+        currentMaterial = mRenderer.material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        offSet += incrementOffset;
+
+        currentMaterial.SetTextureOffset("_MainTex", new Vector2(offSet * speed, 0));
+
+
+	}
+}
